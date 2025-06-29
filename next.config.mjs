6df47f7845dd65ada82a7631016f8ef0dd01f2cd
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-29 21:53:11 +0900

    image url remotePatterns 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/codeitmall/**", // 모든 하위 이미지 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
