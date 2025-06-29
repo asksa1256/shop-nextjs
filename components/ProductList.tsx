434b9540e5f4536750c637ff495e81aab6188226
asksa1256@gmail.com <asksa1256@gmail.com>
2025-06-29 22:11:14 +0900

    상품 목록 추가 + 검색 연동

---
+++ components/ProductList.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "@/components/ProductList.module.css";
+import Image from "next/image";
+
+interface Product {
+  id: number;
+  imgUrl: string;
+  name: string;
+  price: number;
+}
+
+interface ProductListProps {
+  products: Product[];
+}
+
+export default function ProductList({ products = [] }: ProductListProps) {
+  return (
+    <ul className={styles.productList}>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link className={styles.product} href={`/products/${product.id}`}>
+            <Image
+              src={product.imgUrl}
+              width="300"
+              height="300"
+              alt={product.name}
+            />
+            <span className={styles.productName}>{product.name}</span>
+            <br />
+            {product.price}원
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import SearchForm from "@/components/SearchForm";
+import ProductList from "@/components/ProductList";
+import axios from "@/lib/axios";
 
 export default function Search() {
+  const [products, setProducts] = useState();
   const router = useRouter();
   const { q } = router.query;
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
+
   return (
     <section>
       <h2>검색 페이지</h2>
       <SearchForm initialValue={typeof q === "string" ? q : ""} />
       <h3>{q} 검색 결과</h3>
+      <ProductList products={products} />
     </section>
   );
 }
